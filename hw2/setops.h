b6bea78055ae15a74f6830247de98cd0e67f273d
Ezra Magaram <ezramagaram@outlook.com>
2020-06-08 18:17:13 -0700

    got makefile working

--- hw2/setops.h
+++ hw2/setops.h
@@ -1,5 +1,7 @@
 #ifndef SETOPS_H
 #define SETOPS_H
+#include <set>
+#include <string>
     // Computes the intersection of s1 and s2
     std::set<std::string> operator&(const std::set<std::string>& s1, 
     const std::set<std::string>& s2);
