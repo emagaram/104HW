b965242ff9ae68717b847211b362292817ac18ac
Ezra Magaram <ezramagaram@outlook.com>
2020-06-08 18:41:33 -0700

    deleted comments

--- hw2/setops.h
+++ hw2/setops.h
@@ -14,7 +14,7 @@
 
     // Computes the difference of s1 - s2
     std::set<std::string> operator-(const std::set<std::string>& s1,
-    const std::set<std::string>& s2); 
+    const std::set<std::string>& s2);
 #endif
  
 
