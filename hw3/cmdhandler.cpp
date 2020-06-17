3215a92c5ee043bb702f5cb859407c7c4cf5739f
Ezra Magaram <ezramagaram@outlook.com>
2020-06-16 23:04:06 -0700

    Small changes

--- hw3/cmdhandler.cpp
+++ hw3/cmdhandler.cpp
@@ -1,5 +1,6 @@
 #include "cmdhandler.h"
 #include "util.h"
+#include <sstream>
 using namespace std;
 
 QuitHandler::QuitHandler()
@@ -18,6 +19,7 @@ bool QuitHandler::canHandle(const std::string &cmd) const
 
 Handler::HANDLER_STATUS_T QuitHandler::process(TwitEng *eng, std::istream &instr) const
 {
+	std::cout <<"Reading QUIT!!!!!!!!!!!!!!!!!!!";
 	eng->dumpFeeds();
 	return HANDLER_QUIT;
 }
@@ -25,15 +27,38 @@ Handler::HANDLER_STATUS_T QuitHandler::process(TwitEng *eng, std::istream &instr
 AndHandler::AndHandler()
 {
 }
-AndHandler::AndHandler(Handler *next)
+AndHandler::AndHandler(Handler *next) : Handler (next)
 {
 }
 
 bool AndHandler::canHandle(const std::string &cmd) const
 {
+	return cmd == "AND";
+	
 }
 Handler::HANDLER_STATUS_T AndHandler::process(TwitEng *eng, std::istream &instr) const
 {
+	std::string word;
+	std::string line;
+	std::vector<string> *terms;
+	std::vector<Tweet*> output;
+	std::getline(instr, line);
+
+	std::stringstream ss(line);
+
+	
+	 while (ss>>word)
+	 {
+		 std::cout<<"RAN";
+	 	//terms->insert(word);
+	 }
+	std::cout<<terms->size();
+	output = eng->search(*terms, 0);
+	
+	if(output.size()==0){
+		std::cout<<"No matches.";
+	}
+	return HANDLER_OK;
 }
 
 OrHandler::OrHandler()
@@ -45,9 +70,12 @@ OrHandler::OrHandler(Handler *next)
 
 bool OrHandler::canHandle(const std::string &cmd) const
 {
+	return cmd == "OR";
 }
 Handler::HANDLER_STATUS_T OrHandler::process(TwitEng *eng, std::istream &instr) const
 {
+	std::cout << "HOOOO";
+	return HANDLER_OK;
 }
 
 TweetHandler::TweetHandler()
@@ -59,7 +87,9 @@ TweetHandler::TweetHandler(Handler *next)
 
 bool TweetHandler::canHandle(const std::string &cmd) const
 {
+	return cmd == "TWEET";
 }
 Handler::HANDLER_STATUS_T TweetHandler::process(TwitEng *eng, std::istream &instr) const
 {
+	return HANDLER_OK;
 }
