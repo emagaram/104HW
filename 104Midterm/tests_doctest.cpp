943b9387686e747d873d5d93a1b24c56f3daba86
Ezra Magaram <ezramagaram@outlook.com>
2020-06-23 13:30:13 -0700

    Midterm template

---
+++ 104Midterm/tests_doctest.cpp
@@ -0,0 +1,22 @@
+#include "math.h"
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+TEST_SUITE("Prob1")
+{
+    TEST_CASE("Name1")
+    {
+    }
+
+    TEST_CASE("Name2")
+    {
+    }
+}
+
+TEST_SUITE("Prob2")
+{
+    TEST_CASE("Name1")
+    {
+    }
+}
---
+++ 104Midterm/tests_gtest.cpp
@@ -0,0 +1,19 @@
+#include "gtest/gtest.h"
+
+namespace
+{
+    TEST(Prob1, Name1)
+    {
+
+    }
+
+    TEST(Prob1, Name2)
+    {
+    }
+
+    TEST(Prob1, Name3)
+    {
+        
+    }
+
+} // namespace
