267c91d2ab3d40d92f622c59ac918ea8e67b764c
Ezra Magaram <ezramagaram@outlook.com>
2020-07-18 19:27:14 -0700

    might fix

--- hw7/avlbst.h
+++ hw7/avlbst.h
@@ -297,7 +297,7 @@ void AVLTree<Key,Value>::insertFix(AVLNode<Key, Value> *parent, AVLNode<Key, Val
                    gp->setBalance(0);
                 }
                 else{
-                   parent->setBalance(1);
+                   parent->setBalance(-1);
                    gp->setBalance(0);
                    node->setBalance(0); 
                 }            
