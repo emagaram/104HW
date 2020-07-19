80b571e45beda62d335cb57a0ad2f1e74f2b9402
Ezra Magaram <ezramagaram@outlook.com>
2020-07-18 20:50:46 -0700

    s

--- hw7/avlbst.h
+++ hw7/avlbst.h
@@ -287,18 +287,18 @@ void AVLTree<Key,Value>::insertFix(AVLNode<Key, Value> *parent, AVLNode<Key, Val
             {
                 rotateRight(parent);
                 rotateLeft(gp);               
-                if(node->getBalance()==-1){
-                   parent->setBalance(-1);
-                   gp->setBalance(0);
-                   node->setBalance(0);                    
+                if(node->getBalance()==1){
+                   parent->setBalance(0);
+                   gp->setBalance(-1);
+                   node->setBalance(0); 
                 }
                 else if(node->getBalance()==0){
                    parent->setBalance(0);
                    gp->setBalance(0);
                 }
-                else if (node->getBalance()==1){
-                   parent->setBalance(0);
-                   gp->setBalance(1);
+                else if (node->getBalance()==-1){
+                   parent->setBalance(1);
+                   gp->setBalance(0);
                    node->setBalance(0); 
                 }
             }
